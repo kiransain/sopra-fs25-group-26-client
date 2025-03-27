766dc54806c383b525267f38451f7e5544e494b4
GentJash <gent.jashari@uzh.ch>
2025-03-27 17:03:09 +0100

    map rendering with apiservice usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 "use client";
 
+import { useApi } from "@/hooks/useApi";  
+import { useEffect, useState } from "react";
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
-import { useEffect, useState } from 'react';
-import { getApiDomain } from "@/utils/domain";
 
 export default function Page() {
   const [currentLocation, setCurrentLocation] = useState<google.maps.LatLngLiteral | null>(null);
   const [apiKey, setApiKey] = useState<string | null>(null);
+  const apiService = useApi();  
 
   useEffect(() => {
-    const apiUrl = `${getApiDomain()}/api/maps/key`; // Use dynamic API URL
+    
+    const fetchApiKey = async () => {
+      try {
+        
+        const response = await apiService.get<{ apiKey: string }>("/api/maps/key");
+        
+        if (response && response.apiKey) {
+          setApiKey(response.apiKey); 
+        }
+      } catch (error) {
+        console.error("Error fetching API key:", error);
+      }
+    };
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => setApiKey(data.apiKey))
-      .catch(error => console.error("Error fetching API key:", error));
+    fetchApiKey(); 
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -33,7 +43,7 @@ export default function Page() {
       console.log('Geolocation is not supported by this browser.');
       setCurrentLocation({ lat: -33.860664, lng: 151.208138 });
     }
-  }, []);
+  }, [apiService]);
 
   if (!currentLocation || !apiKey) {
     return <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
